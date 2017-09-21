8566a85a1076b18eab96f16bd12cb3656aa808ec
littlejkim <littlejkim@gmail.com>
2017-09-21 01:49:33 -0400

    JSON Parsing : https://www.simplifiedcoding.net/android-json-parsing-tutorial/

--- application/src/main/java/com/example/app/LikedSongsActivity.java
+++ application/src/main/java/com/example/app/LikedSongsActivity.java
@@ -1,10 +1,19 @@
 package com.example.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 import com.example.good_morning.R;
@@ -16,14 +25,70 @@ import com.example.good_morning.R;
 public class LikedSongsActivity extends AppCompatActivity {
 
     public Button toRecommenededSongsButton;
+    public ListView listView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.liked_songs_activity);
+        setContentView(R.layout.activity_main);
 
-        ////recommended songs are a recyclerview below liked songs,
-        // probably in a different color
+        listView = (ListView) findViewById(R.id.listView);
+        getJSON("https://ist402wang.ist.psu.edu/team11/app/getphp.php");
+    }
+
+
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        String[] iris = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            iris[i] = obj.getString("name");
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, iris);
+        listView.setAdapter(arrayAdapter);
     }
 
 }
